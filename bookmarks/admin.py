3e1cdb95133e94759fd18c9fc33cb8bb2d8247b4
Atif85 <atif58136@gmail.com>
2026-08-31 18:21:58 +0500

    Add Folder and Bookmark models.

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from bookmarks.models import User
+from bookmarks.models import User, Folder, Bookmark
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Folder)
+admin.site.register(Bookmark)
--- bookmarks/models.py
+++ bookmarks/models.py
@@ -4,3 +4,35 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Folder(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="folders")
+
+    parent = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.CASCADE, related_name="children"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+
+    folder = models.ForeignKey(
+        Folder,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="bookmarks",
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    favicon_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
